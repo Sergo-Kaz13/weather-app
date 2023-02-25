45bc007e5fde63d27390252cb4edab0140c50fd7
Sergo_Kaz <wwe619dx@ukr.net>
2023-02-25 18:09:44 +0200

    make the given schedule

--- main.js
+++ main.js
@@ -1,13 +1,25 @@
+let maxTemp = 0;
+let minTemp = 0;
+let weatherDays = null;
+
 const graphData = document.querySelector(".graph-data");
 const dayInfoTabs = document.querySelector(".day-info-tabs");
 
 const tempPlusUl = document.createElement("ul");
 const tempMinusUl = document.createElement("ul");
+const timeBlock = document.createElement("ul");
 const waterLine = document.createElement("div");
+const windBlock = document.createElement("ul");
+const popBlock = document.createElement("ul");
 
 tempPlusUl.classList.add("temp-plus");
 tempMinusUl.classList.add("temp-minus");
+timeBlock.classList.add("time-block");
 waterLine.classList.add("water-line");
+windBlock.classList.add("wind-block");
+popBlock.classList.add("pop-block");
+
+getWeatherData();
 
 dayInfoTabs.addEventListener("click", (e) => {
   let target = e.target;
@@ -16,19 +28,19 @@ dayInfoTabs.addEventListener("click", (e) => {
   onActiveTabs(target);
 });
 
-let maxTemp = 0;
-let minTemp = 0;
-
-fetch("./forecast-v1.json")
-  .then((res) => res.json())
-  .then((data) => {
-    const { list } = data;
-    console.log(list);
-    const weatherDays = sortDataByDays(list);
-    maxMinTempPerDay(weatherDays[0]);
+async function getWeatherData() {
+  let response = await fetch("./forecast-v1.json");
+  if (response.ok) {
+    let json = await response.json();
+    const { list } = json;
+    weatherDays = sortDataByDays(list);
+    console.log(weatherDays);
     showGraphDataTemp(weatherDays[0]);
-    console.log(["weatherDays"], weatherDays);
-  });
+    showCurrentDate(weatherDays[0]);
+  } else {
+    console.log("Помилка HTTP: " + response.status);
+  }
+}
 
 function sortDataByDays(dataWeather) {
   const sortDays = [];
@@ -64,11 +76,17 @@ function sortDataByDays(dataWeather) {
 }
 
 function showGraphDataTemp(weatherDay) {
-  weatherDay.forEach(({ main: { temp } }) => {
+  graphData.innerHTML = "";
+  tempMinusUl.innerHTML = "";
+  tempPlusUl.innerHTML = "";
+  timeBlock.innerHTML = "";
+
+  weatherDay.forEach(({ main: { temp }, dt_txt }) => {
     const h = calculateTheColumnHeight(temp);
     temp = transformKelvinInCelsius(temp);
     const showTempPlus = temp > 0 ? true : "";
     const showTempMinus = temp < 0 ? true : "";
+    const time = returnHour(dt_txt);
 
     const plus = h > 0 ? h : 0;
     const minus = h < 0 ? -h : 0;
@@ -82,15 +100,65 @@ function showGraphDataTemp(weatherDay) {
     tempMinusUl.innerHTML += `
       <li style="height:${minus}px;">${
       showTempMinus && `<span class="data-temp-minus">${temp}</span>`
-    }</li>
+    }
+    </li>
+    `;
+
+    timeBlock.innerHTML += `
+      <li>${time}</li>
     `;
   });
-
-  graphData.innerHTML = ``;
 
   graphData.append(tempPlusUl);
   graphData.append(waterLine);
   graphData.append(tempMinusUl);
+  graphData.append(timeBlock);
+}
+
+function showGraphDataWind(weatherDay) {
+  graphData.innerHTML = "";
+  windBlock.innerHTML = "";
+
+  weatherDay.forEach((data) => {
+    const {
+      wind: { speed, deg },
+      dt_txt,
+    } = data;
+
+    const time = returnHour(dt_txt);
+
+    windBlock.innerHTML += `
+      <li>
+        <span>${speed}</span>
+        <img src="./images/arrow-up_icon-icons.com_72374.svg" alt="" style="transform: rotate(${deg}deg);" />
+        <span>${time}</span>
+      </li>
+    `;
+  });
+
+  graphData.append(windBlock);
+}
+
+function showGraphDataPop(weatherDay) {
+  graphData.innerHTML = "";
+  popBlock.innerHTML = "";
+
+  weatherDay.forEach((data) => {
+    const { pop, dt_txt } = data;
+    let height = pop ? pop * 100 : 0;
+    let size = pop ? "%" : "";
+    popData = pop ? pop * 100 : 0;
+    let time = returnHour(dt_txt);
+
+    popBlock.innerHTML += `
+      <li style="height: ${height}px">
+        <span>${popData} ${size}</span>
+        <span>${time}</span>
+      </li>
+    `;
+  });
+
+  graphData.append(popBlock);
 }
 
 function maxMinTempPerDay(weatherDay) {
@@ -136,14 +204,22 @@ function onActiveTabs(target) {
 
     switch (targetId) {
       case "temp":
-        console.log(targetId);
+        showGraphDataTemp(weatherDays[0]);
         break;
       case "pop":
-        console.log(targetId);
+        showGraphDataPop(weatherDays[3]);
         break;
       case "wind":
-        console.log(targetId);
+        showGraphDataWind(weatherDays[0]);
         break;
     }
   }
+}
+
+function returnHour(time) {
+  return time.slice(10, 16);
+}
+
+function showCurrentDate(currentDate) {
+  console.log(currentDate);
 }
